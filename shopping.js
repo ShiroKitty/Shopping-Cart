a6e0c11ac68c89a9db07804fb8a2837bf77c5501
Shiro <liggettt@otc.edu>
2017-11-23 19:51:21 -0600

    Some updates

--- shopping.js
+++ shopping.js
@@ -1,7 +1,18 @@
 var start = new Vue({
-    el: '#display',
+    el: '#cart',
     data: {
-        seen: false,
-        message: "Beluga!"
+        itemsInCart: 0,
+        cartItems: 
+        [
+            {name: 'RimWorld', numOrdered: 0, price: 29.99, total: 0, imgSrc: 'http://cdn.akamai.steamstatic.com/steam/apps/294100/header.jpg?t=1484073238'},
+            {name: 'Civilization VI', numOrdered: 0, price: 59.99, total: 0, imgSrc: 'http://cdn.akamai.steamstatic.com/steam/apps/289070/header.jpg?t=1510266672'},
+            {name: 'Undertale', numOrdered: 0, price: 9.99, total: 0, imgSrc: 'http://cdn.akamai.steamstatic.com/steam/apps/391540/header.jpg?t=1503525486'},
+            {name: 'Wasteland 2', numOrdered: 0, price: 39.99, total: 0, imgSrc: 'http://cdn.akamai.steamstatic.com/steam/apps/240760/header.jpg?t=1504032524'},
+            {name: 'Star Wars: Knights of the Old Republic 2', numOrdered: 0, total: 0, price: 9.99, imgSrc: 'http://cdn.akamai.steamstatic.com/steam/apps/208580/header.jpg?t=1489550586'},
+            {name: 'Stardew Valley', numOrdered: 0, price: 14.99, total: 0, imgSrc: 'http://cdn.akamai.steamstatic.com/steam/apps/413150/header.jpg?t=1493674185'}
+        ]
+    },
+    methods: {
+
     }
 });
